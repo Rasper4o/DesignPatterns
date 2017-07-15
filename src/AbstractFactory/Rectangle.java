18715d1ce3b1f2f98affbfed2aa9ea71f9cc8135
Rasper4o <gsimeonov28@gmail.com>
2017-07-15 16:48:21 -0400

    abstract factory pattern

---
+++ src/AbstractFactory/Rectangle.java
@@ -0,0 +1,14 @@
+package AbstractFactory;
+
+/**
+ * Created by Georgi on 15.7.2017 г..
+ */
+public class Rectangle implements Shape {
+
+    @Override
+    public void draw()
+    {
+        System.out.println(" Rectangle implementation of draw() method.");
+    }
+
+}
